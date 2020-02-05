050668ca76547cc8963a8512a77d20a67114aea5
Kim Youngmin <ymkim50@me.com>
2020-02-05 17:15:50 +0900

    Check OS for image extensions

--- Source/Mac/NSImage+Extensions.swift
+++ Source/Mac/NSImage+Extensions.swift
@@ -1,36 +1,40 @@
+#if os(OSX)
+
 import AppKit
 
 /// Helper UIImage extension.
 extension NSImage {
-  /// Checks if image has alpha component
-  var hasAlpha: Bool {
-    var imageRect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+    /// Checks if image has alpha component
+    var hasAlpha: Bool {
+        var imageRect: CGRect = CGRect(x: 0, y: 0, width: size.width, height: size.height)
 
-    guard let imageRef = cgImage(forProposedRect: &imageRect, context: nil, hints: nil) else {
-      return false
+        guard let imageRef = cgImage(forProposedRect: &imageRect, context: nil, hints: nil) else {
+            return false
+        }
+
+        let result: Bool
+        let alpha = imageRef.alphaInfo
+
+        switch alpha {
+        case .none, .noneSkipFirst, .noneSkipLast:
+            result = false
+        default:
+            result = true
+        }
+
+        return result
     }
 
-    let result: Bool
-    let alpha = imageRef.alphaInfo
+    /// Convert to data
+    func cache_toData() -> Data? {
+        guard let data = tiffRepresentation else {
+            return nil
+        }
 
-    switch alpha {
-    case .none, .noneSkipFirst, .noneSkipLast:
-      result = false
-    default:
-      result = true
+        let imageFileType: NSBitmapImageRep.FileType = hasAlpha ? .png : .jpeg
+        return NSBitmapImageRep(data: data)?
+            .representation(using: imageFileType, properties: [:])
     }
-
-    return result
-  }
-
-  /// Convert to data
-  func cache_toData() -> Data? {
-    guard let data = tiffRepresentation else {
-      return nil
-    }
-
-    let imageFileType: NSBitmapImageRep.FileType = hasAlpha ? .png : .jpeg
-    return NSBitmapImageRep(data: data)?
-      .representation(using: imageFileType, properties: [:])
-  }
 }
+
+#endif
